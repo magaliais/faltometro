d7ef517ccdb816a5fcb825e6a6dbea6e1a3f004c
magaliais <gabriel.ashm@hotmail.com>
2024-07-15 23:32:02 -0300

    📦 feat: create modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import { Dialog, DialogPanel } from "@headlessui/react";
+import React from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+}
+
+const Modal = ({ isOpen, setIsOpen, children }: ModalProps) => {
+  return (
+    <Dialog
+      open={isOpen}
+      as="dialog"
+      className="relative z-10 focus:outline-none"
+      onClose={() => setIsOpen(false)}
+    >
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-zinc-700/40">
+        <div className="flex min-h-full items-center justify-center p-4">
+          <DialogPanel
+            transition
+            className="w-full max-w-md rounded-xl bg-zinc-800 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 shadow-md"
+          >
+            {children}
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default Modal;
